e37a7ffee0e7dfc5798eb5b0c5a4df113538136f
fani62762 <132206539+fani62762@users.noreply.github.com>
2023-05-05 16:09:07 +0300

    admin

--- backend/src/routes/adminRouter.js
+++ backend/src/routes/adminRouter.js
@@ -1,6 +1,5 @@
 const express=require('express');
-const {   forgotPassword ,submitCredentials,updateAdmin, getAdmin, updateadminimg} = require('../controllers/adminControllers');
-
+const {forgotPassword ,submitCredentials,updateAdmin, getAdmin, updateadminimg} =require('../controllers/adminControl');
 const adminrouter=express.Router()
 
 adminrouter.get('/2/:name',getAdmin);
@@ -9,4 +8,4 @@ adminrouter.put('/2/:name',updateAdmin );
 adminrouter.put('/1/:name',updateadminimg);
 adminrouter.post('/forgot-password', forgotPassword);
 
-module.exports=adminrouter;//;
+module.exports=adminrouter;
