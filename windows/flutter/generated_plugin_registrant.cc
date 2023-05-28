8206864db1c87f34cfdd1f8af9404c07b8edd736
fani62762 <132206539+fani62762@users.noreply.github.com>
2023-05-28 21:50:41 +0300

    hh

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,14 +6,11 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <firebase_core/firebase_core_plugin_c_api.h>
 #include <geolocator_windows/geolocator_windows.h>
 #include <modal_progress_hud_nsn/modal_progress_hud_nsn_plugin.h>
 #include <record_windows/record_windows_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FirebaseCorePluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
   GeolocatorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("GeolocatorWindows"));
   ModalProgressHudNsnPluginRegisterWithRegistrar(
